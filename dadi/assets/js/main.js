cf4d48c82408e1e0c198b8ac4a68f66fdbed7966
MicheleBertoia2 <michelebertoia95@gmail.com>
2023-03-23 17:09:19 +0100

    [add] stile e inizio logica

---
+++ dadi/assets/js/main.js
@@ -0,0 +1,12 @@
+const numComputer = document.getElementById('numComputer');
+const numPlayer = document.getElementById('numPlayer');
+const arrayPlayer = [];
+const arrayComputer = [];
+const gioca = document.getElementById('gioca');
+
+gioca.addEventListener('click', function(){
+  const randomPlayer = Math.floor(Math.random() * 6 + 1);
+})
+
+
+
