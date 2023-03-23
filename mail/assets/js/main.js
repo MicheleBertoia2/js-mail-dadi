222f332b1ab5c2f2267370b860feefd08d4fbc92
MicheleBertoia2 <michelebertoia95@gmail.com>
2023-03-23 18:44:55 +0100

    [add] successo se puoi entrare, ma manca messaggio singolo di errore

---
+++ mail/assets/js/main.js
@@ -0,0 +1,29 @@
+const utenti = [
+  'luciarossi@boolmail.com',
+  'lucarossi@boolmail.com',
+  'lucianorossi@boolmail.com',
+  'gianniperignocco@boolmail.com',
+  'adminadmin@boolmail.com',
+  'giorgioverdi@boolmail.com',
+  'michelepago@boolmail.com',
+  'rosaculetto@boolmail.com',
+  'domenicamelalavo@boolmail.com',
+  'hahaha@boolmail.com',
+  'hofinitoleidee@boolmail.com',
+  'eoracosascrivo@boolmail.com',
+]
+
+const buttonLogin = document.getElementById('login');
+const inputMail = document.getElementById('email');
+
+buttonLogin.addEventListener('click', function(){
+  let accessoNegato = true;
+  const mail = inputMail.value;
+  for (let i = 0; i < utenti.length; i++) {
+    const utenteConAccesso = utenti[i];
+    if(mail === utenteConAccesso){      
+      accessoNegato = false;
+      alert('bentornato');
+    }    
+  }; 
+})  
