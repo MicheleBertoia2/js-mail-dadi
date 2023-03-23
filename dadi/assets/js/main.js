c5447704f16ba81431195778a264c097c13cd00f
MicheleBertoia2 <michelebertoia95@gmail.com>
2023-03-23 17:19:45 +0100

    [add] game dadi

--- dadi/assets/js/main.js
+++ dadi/assets/js/main.js
@@ -1,11 +1,19 @@
 const numComputer = document.getElementById('numComputer');
 const numPlayer = document.getElementById('numPlayer');
-const arrayPlayer = [];
-const arrayComputer = [];
 const gioca = document.getElementById('gioca');
 
 gioca.addEventListener('click', function(){
   const randomPlayer = Math.floor(Math.random() * 6 + 1);
+  const randomComputer = Math.floor(Math.random() * 6 + 1);
+  numPlayer.innerHTML = randomPlayer;
+  numComputer.innerHTML = randomComputer;
+  if(randomPlayer < randomComputer){
+    alert('Hai perso!')
+  }else if(randomPlayer > randomComputer){
+    alert('Hai Vinto!')
+  }else{
+    alert('Hai pareggiato')
+  }
 })
 
 
